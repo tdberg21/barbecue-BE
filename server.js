f583fff914c7b9dc9edb92f4f311c8671fd8884e
Tory Dannenberg <rfdberg@gmail.com>
2018-07-22 20:22:55 -0600

    Add get method to return users restaurants

--- server.js
+++ server.js
@@ -4,6 +4,21 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
+app.use((request, response, next) => {
+  response.header('Access-Control-Allow-Origin', '*')
+  next()
+})
+
+app.use((request, response, next) => {
+  response.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next()
+});
+
+app.use((request, response, next) => {
+  response.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers')
+  next()
+})
+
 app.get('/api/v1/users', (request, response) => {
   database('users').select()
     .then((user) => {
@@ -45,15 +60,17 @@ app.post('/api/v1/users/new', (request, response) => {
 
 app.post('/api/v1/users', async (request, response) => {
   const guest = request.query;
+  console.log('request body:', request.body)
   try {
     const users = await database('users').select()
+    console.log('trying')
     const validation = users.find(user => {
       return (user.email === guest.email) && (user.password === guest.password)
     })
     response.status(201).json({ username: validation.username, id: validation.id })
   } catch (error) {
     console.log(error)
-    response.status(500).json('Incorrect email or password', error)
+    response.status(500).json({'Incorrect email or password': error})
   }
 })
 
@@ -75,6 +92,17 @@ app.post('/api/v1/restaurants', (request, response) => {
     .catch(error => {
       response.status(500).json({ error });
     })
+});
+
+app.get('/api/v1/restaurants/:id', async (request, response) => {
+  const id = parseInt(request.params.id)
+  try {
+    const restaurants = await database('restaurants').select()
+    const userFaves = restaurants.filter(restaurant => restaurant.user_id === id)
+    response.status(200).json(userFaves)
+  } catch (error) {
+    response.status(500).json(error)
+  }
 });
 
 app.listen(3000, () => {
